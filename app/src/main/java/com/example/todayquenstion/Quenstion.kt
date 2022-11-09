00462179072e8f988096e3db7143a469e5b096a7
yooeee <isz06188@naver.com>
2022-11-09 09:52:00 +0900

    레이아웃 만들기

---
+++ app/src/main/java/com/example/todayquenstion/Quenstion.kt
@@ -0,0 +1,4 @@
+package com.example.todayquenstion
+
+class Quenstion(val quenstion_title: String, val wonder: String,val wonder_int: String
+                , val date: String,val writter:String)
--- app/src/main/java/com/example/todayquenstion/UserCommuFragment.kt
+++ app/src/main/java/com/example/todayquenstion/UserCommuFragment.kt
@@ -1,17 +1,20 @@
 package com.example.todayquenstion
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.todayquenstion.databinding.ActivityMainhomeBinding
 import com.example.todayquenstion.databinding.FragmentCommunityBinding
-import com.example.todayquenstion.databinding.FragmentMyhomeBinding
 
 class UserCommuFragment : Fragment() {
+
+
+    var list = arrayListOf<Quenstion>()
+
+
+
+
 
 
 
@@ -20,6 +23,15 @@ class UserCommuFragment : Fragment() {
         // 1. 뷰 바인딩 설정
         val binding = FragmentCommunityBinding.inflate(inflater, container, false)
         // 나의메뉴 클릭시 프래그먼트 이동
+
+
+
+        list.add(Quenstion("Q. 프레임워크와 라이브러리 차이점이 무엇인가요?","나도 궁금해요"
+        ,"3","2022-11-01","인하컴돌이"))
+
+
+
+
 
 
 
@@ -38,4 +50,12 @@ class UserCommuFragment : Fragment() {
 
 
     }
+
+
+
+
+
+
+
+
 }
